148767db93de739b7a793f16c22b29967e35ab6b
samarth44600 <subedi42samarth@gmail.com>
2022-01-31 21:55:02 +0545

    Added data

--- src/Components/Directory.jsx
+++ src/Components/Directory.jsx
@@ -1,9 +1,44 @@
 import React from "react";
-
+import directoryData from "../Utils/Data";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFolder, faFile } from "@fortawesome/free-solid-svg-icons";
 export default function Directory() {
+  let looping = (data) => data.name;
+  const rootContent = directoryData.content;
+
+  //   const insideRoot = rootContent.map(looping);
+  let icons;
+  let mapping = rootContent.map((data) => {
+    const iconCheck = data.isFolder;
+    if (iconCheck === true) {
+      icons = <FontAwesomeIcon icon={faFolder} />;
+      console.log(true);
+    } else {
+      icons = <FontAwesomeIcon icon={faFile} />;
+      console.log(false);
+    }
+    rootContent.map(looping);
+  });
+
   return (
     <div>
-      <h1>Directory</h1>
+      {directoryData.name}
+
+      <div>
+        {icons}
+        {/* <p>{insideRoot}</p> */}
+        {mapping}
+        {/* {
+            iconCheck = data.isFolder;
+            if (iconCheck === true) {
+              icons = <FontAwesomeIcon icon={faFolder} />
+            //   {console.log(true);}
+            } else {
+              icons = <FontAwesomeIcon icon={faFile} />
+            //   console.log(false);
+            }
+          }); */}
+      </div>
     </div>
   );
 }
---
+++ src/Utils/Data.js
@@ -0,0 +1,38 @@
+const directoryData = {
+    name: "root",
+    isFolder: true,
+    content: [
+      {
+        name: "public",
+        isFolder: true,
+        content: [
+          { name: "index.html", isFolder: false },
+          { name: "favicon.ico", isFolder: false },
+          { name: "manifest.json", isFolder: false },
+        ],
+      },
+      {
+        name: "src",
+        isFolder: true,
+        content: [
+          {
+            name: "styles",
+            isFolder: true,
+            content: [
+              {
+                name: "App.scss",
+                isFolder: false,
+              },
+            ],
+          },
+          { name: "components", isFolder: true, content: [] },
+          { name: "App.jsx", isFolder: false },
+          { name: "App.css", isFolder: false },
+          { name: "index.js", isFolder: false },
+        ],
+      },
+      { name: "package.json", isFolder: false },
+    ],
+  };
+  
+  export default directoryData;
